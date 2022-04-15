de7e0810fc76fdd3c1862c0003c020b693454d2e
aa2il <jattili@yahoo.com>
2022-04-15 14:50:49 -0700

    Port to windoz

---
+++ windoz.bat
@@ -0,0 +1,17 @@
+@echo off
+echo.
+echo Notes about how to run grid plotter on Windoze 10
+echo.
+echo Already should have everything we need installed already
+echo.
+echo This all works as long as we have the Python/data directory
+echo Need to rethink where we store data and how to handle it if we're missing somethin.
+echo    bandmap.py  
+echo.
+echo    pyinstaller --onefile bandmap.py
+echo    dist\bandmap.exe 
+echo.
+echo     Run Inno Setup Compiler & follow the prompts to create an installer
+echo     This installer works on Windoz 10 & Bottles!
+echo.
+   
